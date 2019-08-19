6fbfd58d010a9f5d5c5d12cac4c192448bd41b55
Magnon Souza <mprs@cin.ufpe.br>
2019-08-19 11:55:30 -0300

    react refactor

---
+++ api/README.md
@@ -0,0 +1,4 @@
+# Weltfy
+Weltfy is a project with the purpose of allowing people to listen the most played local musics from a chosen country.
+
+The project is build based on the last 3 weeks Top 200 from Spotify Charts and it filters the result returning a playlist composed only by local artists.
---
+++ webapp/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Inital development stage.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/magsouza/Weltfy"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source code
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
