53f0a501bedbdd0a31221cdc30730d1244e32035
Magnon Souza <mprs@cin.ufpe.br>
2020-01-24 17:54:53 -0300

    fix on the dates interval logic

--- api/utils/date_handler.py
+++ api/utils/date_handler.py
@@ -1,36 +1,24 @@
 import datetime
 
 def get_interval(day):
-    start = ''
-    end = ''
-
     distance_to_friday = day.isoweekday() - 5
-
-    if distance_to_friday >= 0:
+    if distance_to_friday > 0:
         start = day - datetime.timedelta(days=(distance_to_friday + 7)) # start of interval
         end = day - datetime.timedelta(days=distance_to_friday) # end of interval
     else:
         start = day - datetime.timedelta(days=(distance_to_friday + 7 + 7)) # start of interval
         end = day - datetime.timedelta(days=(distance_to_friday + 7)) # end of interval
-    
     start = date_to_str(start)
     end = date_to_str(end)
-
     return f'{start}--{end}'
 
 def date_to_str(date):
-    day = date.day
-    month = date.month
-    year = date.year
-
-    return f'{year}-{month:02d}-{day:02d}'
+    return f'{date.year}-{date.month:02d}-{date.day:02d}'
 
 def get_3weeks():
     last_3weeks = []
     day = datetime.date.today()
-
-    for i in range(3):
+    for _ in range(3):
         last_3weeks.append(get_interval(day))
         day = day - datetime.timedelta(days=7)
-
     return last_3weeks
