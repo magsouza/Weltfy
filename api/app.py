b84630a841d1f3e341042e7567f9926ed84b2500
Magnon Souza <mprs@cin.ufpe.br>
2020-08-14 09:04:22 -0300

    changed proxy to container name

--- api/app.py
+++ api/app.py
@@ -2,8 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 app.secret_key = ""
+app.SERVER_NAME = "api:5000"
 
 from routes import *
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
