3973279a971a892f3b934bf68ffeaf059446c1c7
Magnon Souza <mprs@cin.ufpe.br>
2020-08-16 18:55:43 -0300

    fix host paramenter on app

--- api/app.py
+++ api/app.py
@@ -2,9 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 app.secret_key = ""
-app.SERVER_NAME = "api:5000"
 
 from routes import *
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
